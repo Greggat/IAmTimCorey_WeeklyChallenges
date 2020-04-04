a1acb2bb1c9e4c1633b269dd7eae1e0bbf7e0285
Greggat <greggat96@gmail.com>
2020-04-04 18:44:11 -0500

    Challenge Three - Error Handling - Completed

---
+++ Challenge3_ErrorHandling/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoLibrary;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PaymentProcessor paymentProcessor = new PaymentProcessor();
+            for (int i = 0; i <= 10; i++)
+            {
+                try
+                {
+                    var result = paymentProcessor.MakePayment($"Demo{ i }", i);
+
+                    if (result != null)
+                        Console.WriteLine(result.TransactionAmount);
+                    else
+                        Console.WriteLine($"Null value for item {i}");
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    Console.WriteLine($"Skipped Invalid record  {e?.InnerException?.Message}");
+                }
+                catch (FormatException e)
+                {
+                    if (i != 5)
+                        Console.WriteLine($"Formatting Issue {e?.InnerException?.Message}");
+                    else
+                        Console.WriteLine($"Payment skipped for payment with {i} items");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Payment skipped for payment with {i} items");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
