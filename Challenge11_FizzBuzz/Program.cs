2f4101575ef923b1be43fd0137263ec2e7d4208d
Greggat <greggat96@gmail.com>
2020-04-09 14:17:11 -0500

    [Completed] Challenge 11 - FizzBuzz

---
+++ Challenge11_FizzBuzz/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenge11_FizzBuzz
+{
+    class FizzBuzzRule
+    {
+        public string text { get; set; }
+        public int divNumber { get; set; }
+    }
+    class Program
+    {
+        static void FizzBuzz(int lowerBound, int upperBound, List<FizzBuzzRule> rules)
+        {
+            for(int i = lowerBound;i <= upperBound;i++)
+            {
+                StringBuilder text = new StringBuilder();
+                foreach(var rule in rules)
+                {
+                    if (i % rule.divNumber == 0)
+                        text.Append(rule.text);
+                }
+                if (text.Length != 0)
+                    Console.WriteLine(text);
+                else
+                    Console.WriteLine(i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<FizzBuzzRule> rules = new List<FizzBuzzRule>();
+            rules.Add(new FizzBuzzRule { text = "Fizz", divNumber = 3 });
+            rules.Add(new FizzBuzzRule { text = "Buzz", divNumber = 5 });
+            rules.Add(new FizzBuzzRule { text = "Jazz", divNumber = 7 });
+
+            FizzBuzz(1, 100, rules);
+
+            Console.ReadLine();
+        }
+    }
+}
